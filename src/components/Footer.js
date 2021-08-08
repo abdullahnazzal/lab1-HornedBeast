d7b4951a4c8b8c6f385634941a773c71ce8e5a36
abdullahnazzal <a.nazzal1995@gmail.com>
2021-08-08 21:49:55 +0300

    Make My First React Web Pages

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Footer extends React.Component{
+    render(){
+        return (
+            <p>ABDULLAH NAZZAL</p>
+        )
+    }
+}
+
+export default Footer;
