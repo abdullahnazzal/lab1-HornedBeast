d9b9e1b6173790ce819a5b0fbb8c022d6c8b362a
abdullahnazzal <a.nazzal1995@gmail.com>
2021-08-09 16:09:08 +0300

    Added feature 3 Bootstrap

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,25 +1,41 @@
 import React from 'react';
-
-class HornedBeasts extends React.Component{
-    constructor(props){
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+class HornedBeasts extends React.Component {
+    constructor(props) {
         super(props);
-        this.state={
-            numberOfVotes:0
+        this.state = {
+            numberOfVotes: 0
         }
     }
-    increaseVotes=()=>{
+    increaseVotes = () => {
         this.setState({
-            numberOfVotes:this.state.numberOfVotes+1
+            numberOfVotes: this.state.numberOfVotes + 1
         })
     }
-    render(){
+    render() {
         return (
+
             <div>
-                <h2> {this.props.title}</h2>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={this.props.imageUrl} />
+                    <Card.Body>
+                        <Card.Title>{this.props.title}</Card.Title>
+                        <Card.Text>
+                        {this.props.description}
+                        </Card.Text>
+                        <Button onClick={this.increaseVotes}>Vote</Button>
+                        <Card.Text>
+                        {this.state.numberOfVotes}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                {/* <h2> {this.props.title}</h2>
                 <img src={this.props.imageUrl} alt={this.props.title} title ={this.props.title} />
                 <p> {this.props.description}</p>
                 <button onClick={this.increaseVotes}>Vote</button>
-                <p> {this.state.numberOfVotes}</p>
+                <p> {this.state.numberOfVotes}</p> */}
             </div>
         )
     }
