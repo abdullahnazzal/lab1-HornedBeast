b3a5b7f622b06391989611b1bc1e2a616a473f0b
abdullahnazzal <a.nazzal1995@gmail.com>
2021-08-09 15:42:47 +0300

    Add Vote feature

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,12 +1,25 @@
 import React from 'react';
 
 class HornedBeasts extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            numberOfVotes:0
+        }
+    }
+    increaseVotes=()=>{
+        this.setState({
+            numberOfVotes:this.state.numberOfVotes+1
+        })
+    }
     render(){
         return (
             <div>
                 <h2> {this.props.title}</h2>
                 <img src={this.props.imageUrl} alt={this.props.title} title ={this.props.title} />
                 <p> {this.props.description}</p>
+                <button onClick={this.increaseVotes}>Vote</button>
+                <p> {this.state.numberOfVotes}</p>
             </div>
         )
     }
